a2617380bf49feba9b974fca023b27d5f6551efb
Sanjay T C <sanjaytc@example.com>
2025-07-23 11:56:43 +0530

    First-time deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -11,9 +14,11 @@ await connectDB()
 //Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 //API Routes
 app.get('/', (req, res)=>res.send('Server is Live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, ()=>console.log(`Server Listening at http://localhost:${port}`));

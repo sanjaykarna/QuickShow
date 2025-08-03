ed6982990ee7b69da335a34fdc8f0527fe697b3a
Sanjay T C <sanjaytc@example.com>
2025-08-03 14:12:45 +0530

    Debugging done for "addShow"

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,66 @@
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js"
+
+
+//Function to check availablity of selected seats for a movie
+const checkSeatAvailablity = async(showId, selectedSeats)=>{
+    try{
+        await Show.findById(showId)
+        if(!showData) return false;
+        const occupiesSeats = showData.occupiesSeats;
+        const isAnySeatTaken = selectedSeats.some(seat=>occupiesSeats[seat]);
+        return !isAnySeatTaken;
+    }catch(error){
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const createBooking = async(req,res)=>{
+    try{
+        const {userId} = req.auth();
+        const {showId,selectedSeats} = req.body;
+        const {origin} = req.headers;
+
+        //check is seat is avalaible for selected show
+        const isAvailable =await checkSeatAvailablity(showId,selectedSeats)
+
+        if(!isAvailable){
+            return res.json({success:false,message:"selected seats are not avalailable"})
+        }
+
+        //get the show details
+        const showData = await Show.findById(showId).populate('movie');
+
+        //create a new booking
+        const booking = await Booking.create({
+            user: userId,
+            show:showId,
+            amount:showData.showPrice * selectedSeats.length,
+            bookedSeats:selectedSeats
+        })
+        selectedSeats.map((seat)=>{
+            showData.occupiedSeats[seat] = userId;
+        })
+        showData.markModified('occupiedSeats');
+        await showData.save();
+
+        //stripe gateway intitialize
+        res.json({success:true,message:'Booked Successfully'})
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+}
+
+export const getOccupiedSeats = async(req, res)=>{
+    try{
+        const {showId} = req.params;
+        const showData = await Show.findById(showId)
+        const occupiedSeats = Object.keys(showData.occupiedSeats)
+        res.json({success:true, occupiedSeats})
+    }catch(error){
+         console.log(error.message);
+         res.json({success:false, message:error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
-
+const port = 3000;
 await connectDB()
 
 //Middleware
@@ -18,6 +22,13 @@ app.use(clerkMiddleware())
 //API Routes
 app.get('/', (req, res)=>res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+app.use('/api/booking',bookingRouter)
+app.use('/api/admin',adminRouter)
+app.use('/api/user',userRouter)
+
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
 
 // Export the app for Vercel
 export default app;

979d35787df661cc6eb0e04637fa190f76edc29e
Sanjay T C <sanjaytc@example.com>
2025-08-09 22:04:06 +0530

    updated password of brev to gmail app password

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -8,6 +8,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+   logger: true,  // add this
+   debug: true, 
 });
 
 const sendEmail = async({to, subject,body})=>{

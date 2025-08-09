2c83bee299a09c2fda67e124d266ccf904b2d3a5
Sanjay T C <sanjaytc@example.com>
2025-08-09 22:57:43 +0530

    updated booking.js

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
     user: { 
-        type: mongoose.Schema.Types.ObjectId, 
+        type: String, 
         ref: 'User', 
         required: true 
     },

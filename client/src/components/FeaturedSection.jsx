e9d144f97e15c4ff3c0a590439891004685464c3
Sanjay T C <sanjaytc@example.com>
2025-08-21 22:25:25 +0530

    adjusted movie cards

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -47,7 +47,7 @@ const FeaturedSection = () => {
             </div>
             
             {/* Movie Grid - For 2 cards */}
-            <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 mt-8 max-w-4xl mx-auto'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8 max-w-3xl mx-auto'>
                 {shows.slice(0, 4).map((show, index) => (
                     <div 
                         key={show._id}
